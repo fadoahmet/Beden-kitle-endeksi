667e670eb46d9148f5c13c5ed8bc273490d0b47a
fadoahmet <102147670+fadoahmet@users.noreply.github.com>
2022-03-24 17:00:21 +0300

    Add files via upload

---
+++ Test.java/Beden_kitle_endeksi.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class test {
+
+    public static void main(String[] args) {
+       
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Lutfen kilonuzu giriniz:");
+        int kilo = scanner.nextInt();
+
+        System.out.print("Lutfen boyunuzu giriniz:(For example:1,72)");
+        double boy = scanner.nextDouble();
+
+        double bki = kilo / (boy * boy);
+        
+        System.out.println("Beden Kitle indeksiniz:" + bki);
+        
+    
+        
+
+            }
+}
