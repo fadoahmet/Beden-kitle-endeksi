3e5b39994a7c4f6911ba2cbbfab361a8fcf367e5
Fadıl Ahmet Tüfekçi <fadoahmet233@gmail.com>
2022-03-29 10:27:17 +0300

    a

---
+++ minimum_sayi_bulma.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class minimum_sayi_bulma {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Birinci sayi: ");
+        int a = scanner.nextInt();
+        System.out.print("Ikinci sayi: ");
+        int b = scanner.nextInt();
+        System.out.print("Ucuncu sayi: ");
+        int c = scanner.nextInt();
+             
+        int min = 2;
+
+        if (a <= b && a <= c) {
+
+             min = a;
+        }
+        else if (b <= a && b <= c) {
+
+            min = b;
+
+        }
+
+         else {
+
+            min = c;
+        }
+
+        System.out.println("Minimmum sayi:" + min);
+
+            
+
+    }
+    
+}
